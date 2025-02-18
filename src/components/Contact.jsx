64c64a36481d00dec585875ea278650d0fb0a1a1
Skulldued <skulldued30@gmail.com>
2025-02-19 00:49:53 +0530

    Till Now Email functionality also setup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+    try {
+      const response = await emailjs.send(
+        serviceId,
+        templateId,
+        data,
+        publicKey
+      );
+      console.log("Success!", response.status, response.text);
+      alert("Message sent successfully!");
+    } catch (error) {
+      console.log("FAILED...", error);
+      alert("Failed to send message. Try again later!");
+    }
+    reset(); // Form reset karein
+  };
   return (
     <div className="py-6 px-4 md:px-0 md:py-20">
       <div className="grid lg:grid-cols-2 gap-7">
@@ -15,64 +42,111 @@ const Contact = () => {
             </p>
           </div>
           <div>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
               <div className="md:flex my-5 justify-between gap-5">
                 <div className="md:w-1/2 my-3 my-lg-0">
                   <input
                     type="text"
+                    {...register("firstName", {
+                      required: "Enter First Name",
+                      pattern: {
+                        value: /^[A-Za-z]+$/i,
+                        message: "Only Alphabets Are Allowed",
+                      },
+                    })}
                     placeholder="Enter First Name"
                     className="border-b py-2 px-3 w-full rounded-md border-slate-700 focus:outline-none bg-transparent text-slate-300"
-                    name=""
-                    id=""
                   />
+                  {errors.firstName && (
+                    <p className="text-red-500 pl-4">
+                      {errors.firstName.message}
+                    </p>
+                  )}
                 </div>
                 <div className="md:w-1/2 my-3 my-lg-0">
                   <input
                     type="text"
+                    {...register("lastName", {
+                      required: "Enter Last Name",
+                      pattern: {
+                        value: /^[A-Za-z]+$/i,
+                        message: "Only Alphabets Are Allowed",
+                      },
+                    })}
                     placeholder="Enter Last Name"
                     className="border-b py-2 px-3 w-full rounded-md border-slate-700 focus:outline-none bg-transparent text-slate-300"
-                    name=""
-                    id=""
                   />
+                  {errors.lastName && (
+                    <p className="pl-4 text-red-500">
+                      {errors.lastName.message}
+                    </p>
+                  )}
                 </div>
               </div>
               <div className="md:flex my-5 justify-between gap-5">
                 <div className="md:w-1/2 my-3 my-lg-0">
                   <input
-                    type="text"
+                    type="email"
+                    {...register("email", {
+                      required: "Email is Required",
+                      pattern: {
+                        value:
+                          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                        message: "Enter a valid email address",
+                      },
+                    })}
                     placeholder="Enter Email"
                     className="border-b py-2 px-3 w-full rounded-md border-slate-700 focus:outline-none bg-transparent text-slate-300"
-                    name=""
-                    id=""
                   />
+                  {errors.email && (
+                    <p className="text-red-500 pl-4">{errors.email.message}</p>
+                  )}
                 </div>
                 <div className="md:w-1/2 my-3 my-lg-0">
                   <input
                     type="text"
+                    {...register("number", {
+                      required: "Mobile Number Required",
+                      pattern: {
+                        value: /^[0-9]{10}$/,
+                        message: "Enter a valid 10-digit phone number",
+                      },
+                    })}
                     placeholder="Enter Phone Number"
                     className="border-b py-2 px-3 w-full rounded-md border-slate-700 focus:outline-none bg-transparent text-slate-300"
-                    name=""
-                    id=""
                   />
+                  {errors.number && (
+                    <p className="text-red-500 pl-4">{errors.number.message}</p>
+                  )}
                 </div>
               </div>
               <div className="my-5">
                 <input
                   type="text"
+                  {...register("subject", {
+                    required: "Enter Subject",
+                    pattern: {
+                      value: /^[A-Za-z ]+$/i,
+                      message: "Only alphabets are allowed",
+                    },
+                  })}
                   placeholder="Enter Subject"
                   className="border-b py-2 px-3 w-full rounded-md border-slate-700 focus:outline-none bg-transparent text-slate-300"
-                  name=""
-                  id=""
                 />
+                {errors.subject && (
+                  <p className="text-red-500 pl-4">{errors.subject.message}</p>
+                )}
               </div>
               <div className="my-5">
                 <textarea
                   type="text"
+                  {...register("message", { required: true })}
                   placeholder="Enter Message"
                   className="border-b py-2 px-3 w-full rounded-md border-slate-700 focus:outline-none bg-transparent text-slate-300"
-                  name=""
-                  id=""
                 />
+                {errors.message && (
+                  <p className="text-red-500 pl-4">Enter Message Here**</p>
+                )}
               </div>
               <div className="my-5">
                 <button className="bg-sky-400 py-2 text-center w-full rounded-sm">
